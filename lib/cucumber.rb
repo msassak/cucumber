f58398d602e1124925ad0709e6aa8e3016748611
Mike Sassak <msassak@gmail.com>
2010-05-07 03:21:32 -0400

    Remove redundant require

--- lib/cucumber.rb
+++ lib/cucumber.rb
@@ -3,7 +3,7 @@ $:.unshift(File.dirname(__FILE__)) unless
 
 require 'yaml'
 require 'cucumber/platform'
-require 'cucumber/parser'
+require 'cucumber/ast'
 require 'cucumber/step_mother'
 require 'cucumber/cli/main'
 require 'cucumber/broadcaster'
