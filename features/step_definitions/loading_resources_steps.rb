7a41e6b3464339844a2b70c55c994f447b9076c0
Mike Sassak <msassak@gmail.com>
2010-06-04 00:21:38 -0400

    Use correct tag

--- features/step_definitions/loading_resources_steps.rb
+++ features/step_definitions/loading_resources_steps.rb
@@ -1,10 +1,10 @@
 Given /^an? \w+ server on localhost:(\d+) is serving the contents of the features directory$/ do |port|
-  @feature_server_pid = fork do
+  @resource_server_pid = fork do
     ResourceServer.run! :host => 'localhost', :port => port, :root => working_dir
   end
 end
 
-After('@feature_server') do
-  Process.kill('TERM', @feature_server_pid)
-  Process.wait(@feature_server_pid, Process::WNOHANG)
+After('@resource_server') do
+  Process.kill('TERM', @resource_server_pid)
+  Process.wait(@resource_server_pid, Process::WNOHANG)
 end
